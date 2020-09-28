ca2eab46124f8d338cb4124c0d6f8c58759ec116
RainNy <permetr35@gmail.com>
2020-09-28 20:52:29 +0300

    Add rest methods to DictionaryController

--- src/main/kotlin/com/regions/dictionary/controllers/DictionaryController.kt
+++ src/main/kotlin/com/regions/dictionary/controllers/DictionaryController.kt
@@ -2,23 +2,39 @@ package com.regions.dictionary.controllers
 
 import com.regions.dictionary.domain.Region
 import com.regions.dictionary.mappers.RegionMapper
+import org.springframework.beans.BeanUtils
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/")
 class DictionaryController {
     @Autowired
     lateinit var regionMapper: RegionMapper
 
-    @GetMapping("/")
-    fun getDictionaryOfRegions(model: Model) : MutableList<Region> {
+    @GetMapping
+    fun getDictionaryOfRegions() : MutableList<Region> {
         val list = ArrayList<Region>()
-        list.addAll(regionMapper.getRegions())
-        model.addAttribute("regions", list)
+        list.addAll(regionMapper.selectRegions())
         return list
+    }
+
+    @PostMapping
+    fun addNewRegionToDictionary(@RequestBody region: Region){
+        regionMapper.insertRegionToDictionary(region)
+    }
+
+    @PutMapping("{id}")
+    fun updateRegionInDictionary(
+            @PathVariable("id") regionFromDatabase: Region,
+            @RequestBody regionFromUser: Region
+    ){
+        BeanUtils.copyProperties(regionFromUser, regionFromDatabase, "id")
+        regionMapper.updateRegionInDictionary(regionFromDatabase)
+    }
+
+    @DeleteMapping("{id}")
+    fun deleteRegionFromDictionaryById(@PathVariable("id") selectedRegion: Region){
+        regionMapper.deleteRegionFromDictionaryById(selectedRegion)
     }
 }
--- src/main/kotlin/com/regions/dictionary/mappers/RegionMapper.kt
+++ src/main/kotlin/com/regions/dictionary/mappers/RegionMapper.kt
@@ -1,15 +1,27 @@
 package com.regions.dictionary.mappers
 
 import com.regions.dictionary.domain.Region
-import org.apache.ibatis.annotations.Mapper
-import org.apache.ibatis.annotations.Param
-import org.apache.ibatis.annotations.Select
+import org.apache.ibatis.annotations.*
 
 @Mapper
 interface RegionMapper {
-    @Select("SELECT * FROM REGIONS WHERE id = #{id}")
-    fun getRegionById(@Param("id") id: Long) : Region
-
+    @Results(
+        Result(property = "name", column = "name"),
+        Result(property = "shortName", column = "shortName"),
+        Result(property = "id", column = "id")
+    )
     @Select("SELECT * FROM REGIONS")
-    fun getRegions() : MutableList<Region>
+    fun selectRegions() : MutableList<Region>
+
+    @Select("SELECT * FROM REGIONS WHERE id = #{id}")
+    fun selectRegionById(@Param("id") id: Long) : Region
+
+    @Insert("INSERT INTO REGIONS(name, shortName) VALUE (#{name}, #{shortName})")
+    fun insertRegionToDictionary(region: Region)
+
+    @Update("UPDATE REGIONS SET name = #{name}, shortName = #{shortName} WHERE id = #{id}")
+    fun updateRegionInDictionary(region: Region)
+
+    @Delete("DELETE FROM REGIONS WHERE id = #{id}")
+    fun deleteRegionFromDictionaryById(@Param("id") regionToDelete: Region)
 }
